7696d60982cca8954610c27d32a3a581d6b5c47e
sen0rxol0 <sen0rxol0@users.noreply.github.com>
2024-01-10 05:09:09 +0100

    Delete AVFoundationSimplePlayer-iOS directory

--- AVFoundationSimplePlayer-iOS/Cell.h
+++
@@ -1,9 +0,0 @@
-
-#import <UIKit/UIKit.h>
-
-@interface Cell : UICollectionViewCell
-
-@property (strong, nonatomic) IBOutlet UIImageView *image;
-@property (strong, nonatomic) IBOutlet UILabel *label;
-
-@end
